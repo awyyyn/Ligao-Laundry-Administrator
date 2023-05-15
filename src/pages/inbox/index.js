a0816494e3fc4ff974599188eb5fa53ace33d340
newbyy <alwin.puche16@gmail.com>
2023-05-15 22:51:47 +0800

    add hero image

---
+++ src/pages/inbox/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LayoutAdmin from '../layouts/adminlayout'
+import { Box } from '@mui/material'
+
+export default function index() {
+    return (
+        <LayoutAdmin>
+            <Box>
+                
+            </Box>
+        </LayoutAdmin>
+    )
+}
