ad540dcf3d5f060d958d0cfd16ba5adcc40f8a32
newbyy <alwin.puche16@gmail.com>
2023-05-17 00:44:10 +0800

    merge

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Input } from './input.jsx';
+export { default as Snackbar } from './Snackbar.jsx';
+export { default as Backdrop } from './backdrop.jsx';
+// export { default as Sidenav } from './sidenav.js';
+export { default as Navbar } from './navbar.jsx';
+export { default as Drawer } from './drawer.jsx'
---
+++ src/customization/index.js
@@ -0,0 +1,39 @@
+import { Snackbar, colors, createTheme } from "@mui/material";
+import * as React from 'react';
+import { Button } from '@mui/material';
+import { styled } from "@mui/material/styles";
+
+
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#00a7cc',
+            main: '#00667e', 
+            dark: '#002a33',
+        }, 
+        Whitecolor: {
+            light: '#FFFFFF',
+            main: '#f6f6f6',
+            dark: '#d0d0d0'
+        }
+    }
+});
+
+const PrimaryBTN = styled(Button)(({ theme }) => ({
+    color: "white",
+    
+    backgroundColor: theme.palette.primary.main,
+    '&:hover, .MuiButton-root ': {
+        color: theme.palette.primary.main, 
+        border: '1px solid', 
+    }, 
+    
+}));
+
+const SnackInfo = styled(Snackbar)(({theme}) => ({
+    
+}))
+
+export { PrimaryBTN }
+export default theme
