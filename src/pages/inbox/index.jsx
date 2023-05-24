277ad6895dfb4250c39d1ffd7c35c6d8656e5b68
newbyy <alwin.puche16@gmail.com>
2023-05-24 15:53:14 +0800

    fixing height in inbox page, 4

--- src/pages/inbox/index.jsx
+++ src/pages/inbox/index.jsx
@@ -54,7 +54,7 @@ export default function Index() {
     /* SEND MESSAGE FUNCTION */
     const sendMessage = async() => {
         if(message == "") return  
-        const {data, error} = await supabase.from('message_channel').insert({sender_id: id, recipent_id: 'admin', message: message}).select();
+        const {data, error} = await supabase.from('message_channel').insert({sender_id: id, recipent_id: 'admin', message: message }).select();
         console.log(data)
         console.log(error?.details)
         setMessage('');
@@ -162,7 +162,7 @@ export default function Index() {
                             }}
                         >     
         
-                            <Box sx={{height: {xs: 'calc(90% - 70px)', sm: '90%'}, overflow: 'hidden'}}>
+                            <Box sx={{height: {xs: 'calc(100% - 70px)', sm: '90%'}, overflow: {xs: 'auto', sm: 'hidden'}}}>
                                 <Stack  sx={{overflow: 'auto', p: 1, height: '100%', display: 'flex', flexDirection: 'column-reverse', }}>
                                     {messages.map((message) => {
 
@@ -194,7 +194,7 @@ export default function Index() {
                                     }
                                 </Stack>
                             </Box>
-                            <Box sx={{height: {xs: '70px', sm: '10%'}, backgroundColor  : {xs: 'red', sm: 'blue'}, borderTop: '1px solid rgba(0, 0, 0, 0.2)', display: 'flex', position: 'relative'}}>
+                            <Box sx={{height: {xs: '70px', sm: '10%'}, /* backgroundColor  : {xs: 'red', sm: 'blue'},  */borderTop: '1px solid rgba(0, 0, 0, 0.2)', display: 'flex', position: 'relative'}}>
                                 <Input 
                                     sx={{fontSize: '2rem', width: '100%', paddingX: 2, paddingRight: 6, overflow: 'auto'}} 
                                     onKeyDown={(e) => e.key == "Enter" && sendMessage()} 
